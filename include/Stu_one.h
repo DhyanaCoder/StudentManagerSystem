298e10b0462a0033e04c8ffda4b668711cae6eeb
bhk <baohekang@126.com>
2020-04-03 10:46:27 +0800

    first version

---
+++ include/Stu_one.h
@@ -0,0 +1,15 @@
+#ifndef STU_ONE_H
+#define STU_ONE_H
+#include "Stu.h"
+
+
+class Stu_one:public Stu
+{
+    public:
+        Stu_one(int stu_id,string name,string sex,BirthTime birthtime,Grade g);
+        Stu_one();
+
+
+
+};
+#endif // STU_ONE_H
---
+++ include/Stu_two.h
@@ -0,0 +1,15 @@
+#ifndef STU_TWO_H
+#define STU_TWO_H
+#include "Stu.h"
+
+class Stu_two:public Stu
+{
+    public:
+        Stu_two(int stu_id,string name,string sex,BirthTime birthtime,Grade g);
+        Stu_two();
+
+
+};
+
+
+#endif // STU_TWO_H
